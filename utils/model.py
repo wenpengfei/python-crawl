4dde102ee8346ef8809991d36e7519ff4653cb28
catwen <jquery.min.js@gmail.com>
2017-01-09 23:38:40 +0800

    feat: First commit

---
+++ utils/model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+class Film:
+  def __init__(self, title, imgUrl, description):
+    self.title = title
+    self.imgUrl = imgUrl
+    self.description = description
