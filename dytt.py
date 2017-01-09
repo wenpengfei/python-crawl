12e735a407bd15909ab65566a76b3a6a1e81b924
catwen <jquery.min.js@gmail.com>
2017-01-10 00:05:41 +0800

    feat: Add spider

--- dytt.py
+++ dytt.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+from utils.model import Film
+from utils.getSoup import getSoup
+
+rootUrl = "http://www.dytt8.net"
+
+soup = getSoup(rootUrl + '/index.html')
+titles = soup.select_one('.co_area2').select('.co_content2 a')
+films = []
+for item in titles:
+    detailUrl = rootUrl+item["href"]
+    detailSoup = getSoup(detailUrl)
+    detailTitle = detailSoup.select_one("div.co_area2 > div.title_all > h1 > font")
+    detailImg = detailSoup.select_one("#Zoom img")
+    detailDownload = detailSoup.select_one("#Zoom table a")
+    if detailImg and detailTitle and detailDownload:
+        films.append(Film(detailTitle.text, detailImg['src'], '-', detailDownload['href']))
+for test in films:
+    print test.title
+    print test.imgUrl
+    print test.description
+    print test.downloadUrl
--- utils/model.py
+++ utils/model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 class Film:
-  def __init__(self, title, imgUrl, description):
+  def __init__(self, title, imgUrl, description, downloadUrl):
     self.title = title
     self.imgUrl = imgUrl
     self.description = description
+    self.downloadUrl = downloadUrl
