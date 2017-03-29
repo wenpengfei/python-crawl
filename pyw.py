a5a632db67cc54d82544514af4d42e3cfda9c0b9
wpf <jquery.min.js@gmail.com>
2017-03-29 18:57:01 +0800

    feat: Edit pyw

--- pyw.py
+++ pyw.py
@@ -15,8 +15,6 @@ DB = CLIENT.EpicWork
 MOVIES = DB.movies
 
 ROOT_URL = "http://pianyuan.net"
-HTMLDOC = get_soup('http://pianyuan.net/mv?type=1&order=update&p=1')
-ITEMSDOCS = HTMLDOC.select('.nopl')
 
 def get_year(source):
     """-"""
@@ -52,16 +50,32 @@ def get_attrs(detail_soup):
         result.append({str(val.text): str(attr_values_formatted[i]).strip()})
     return result
 
+
+
+
+
+
+
+
+
+
+
+
+
+HTMLDOC = get_soup('http://pianyuan.net/mv?order=update&p=1')
+ITEMSDOCS = HTMLDOC.select('.nopl')
+
 for itemdoc in ITEMSDOCS:
     item_soup = BeautifulSoup(str(itemdoc), 'html.parser')
+
+    SOURCE_URL = ROOT_URL + item_soup.select_one('.thumbnail')['href']
+    DETAILSOUP = get_soup(SOURCE_URL)
 
     movie_title = item_soup.select_one('.nobr').text
     movie_main_actor = item_soup.select_one('.info > p').text
     movie_douban_rate = item_soup.select_one('.sum').text
     movie_img_url = item_soup.select_one('img')['data-original']
     movie_year = get_year(item_soup.select_one('.thumbnail')['title'])
-    movie_pyw_detail = ROOT_URL + item_soup.select_one('.thumbnail')['href']
-    DETAILSOUP = get_soup(movie_pyw_detail)
     movie_douban_url = get_douban_url(DETAILSOUP)
     movie_attrs = get_attrs(DETAILSOUP)
     print movie_attrs
