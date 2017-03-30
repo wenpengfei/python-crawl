24cb2f8d20fd4a5f26ebc977f51e86c670c9c245
wpf <jquery.min.js@gmail.com>
2017-03-30 16:03:52 +0800

    feat: Change arr to dic

--- pyw.py
+++ pyw.py
@@ -34,7 +34,7 @@ def get_douban_url(detail_soup):
 
 def get_attrs(detail_soup):
     """-"""
-    result = []
+    result = {}
     attr_names_soup = detail_soup.select('ul.detail strong')
     attr_values_soup = detail_soup.select('ul.detail div')
     attr_values_formatted = []
@@ -49,7 +49,9 @@ def get_attrs(detail_soup):
                 arr_result.append(val.text)
             attr_values_formatted.append(','.join(arr_result))
     for i, val in enumerate(attr_names_soup):
-        result.append({str(val.text): str(attr_values_formatted[i]).strip()})
+        # result.append({str(val.text): str(attr_values_formatted[i]).strip()})
+        result[str(val.text)] = str(attr_values_formatted[i]).strip()
+    print result
     return result
 
 def get_download_url(detail_soup):
@@ -101,7 +103,7 @@ for itemdoc in ITEMSDOCS:
     movie_douban_url = get_douban_url(DETAILSOUP)
     movie_attrs = get_attrs(DETAILSOUP)
     movie_download_url = get_download_url(DETAILSOUP)
-    print movie_title
+    print movie_download_url
     FILMS.append(
         {
             "filmTitle":movie_title,
